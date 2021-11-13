0cfe6c76f47d530b1ef9cb7af268142778856cf3
Bhanu Teja <18211a04h0@bvrit.ac.in>
2021-11-13 16:40:23 +0530

    Resolved

--- Daily-coding-problems/Nov-21/Week-2/09-11/max_2d_sum.cpp
+++ Daily-coding-problems/Nov-21/Week-2/09-11/max_2d_sum.cpp
@@ -6,29 +6,36 @@ using namespace std;
 int main() 
 {
 	int n;
-	ll A[n][n];
+	int i, j;
+	int A[n][n];
 	scanf("%d", &n);
 	for(int i = 0; i < n; i++)
 		for(int j = 0; j < n; j++)
 		{
 			scanf("%d", &A[i][j]);
-			if(i > 0) A[i][j] += A[i-1][j];
-			if(j > 0) A[i][j] += A[i][j-1];
-			if(i > 0 && j > 0) A[i][j] -= A[i-1][j-1];
+			if (i > 0) A[i][j] += A[i - 1][j];
+			if (j > 0) A[i][j] += A[i][j - 1];
+			if (i > 0 && j > 0) A[i][j] -= A[i - 1][j - 1];
 		}
 
-	ll maxSumRect = -127 * 100 * 100;
+	int maxSubRect = -116;
+	int subRect=0;
 	for(int i = 0; i < n; i++)
 		for(int j = 0; j < n; j++)
-			for(int k = 0; k < n; k++)
+			for(int k = i; k < n; k++)
 				for(int l = j; l < n; l++)
 				{
-					ll subRect = A[k][l];
+					subRect = A[k][l];
 					if(i > 0) subRect -= A[i-1][l];
 					if(j > 0) subRect -= A[k][j-1];
 					if(i > 0 && j > 0) subRect += A[i-1][j-1];
 					maxSubRect = max(maxSubRect, subRect);
 				}
-	cout << maxSubRect << "\n";
 
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+			cout << A[i][j] << " ";
+		cout << "\n";
+	}
 }
